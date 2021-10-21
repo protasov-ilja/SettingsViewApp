f1b95a482f253a28abece4bfaa81c0be4b7e5fd5
Ilya Protasov <pfltriot@gmail.com>
2021-10-22 00:11:17 +0300

    Refactored data structure of table view and cells (#3)
    This need to easier add new cell types

---
+++ SettingsViewApp/Sources/App/Model/CellType.swift
@@ -0,0 +1,11 @@
+//
+//  CellType.swift
+//  SettingsViewApp
+//
+//  Created by Ilya Protasov on 20.10.2021.
+//
+
+enum CellType {
+    case defaultCell(withData: DefaultCellData)
+    case switchCell(withData: SwitchCellData)
+}
---
+++ SettingsViewApp/Sources/App/Model/SectionData.swift
@@ -0,0 +1,10 @@
+//
+//  SectionData.swift
+//  SettingsViewApp
+//
+//  Created by Ilya Protasov on 20.10.2021.
+//
+
+struct SectionData {
+    let cells: [CellType]
+}
